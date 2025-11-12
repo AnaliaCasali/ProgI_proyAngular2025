2f7f0328f3a0cf6cd43d72d961d73b6e39aa1b59
Analia <test@gmail.com>
2025-11-12 20:02:02 -0300

    add cdr

--- src/app/features/lista-estudiantes/lista-estudiantes.ts
+++ src/app/features/lista-estudiantes/lista-estudiantes.ts
@@ -1,5 +1,5 @@
 import { Estudiante } from './../../models/estudiante';
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { EstudianteService } from '../../services/estudiante-service';
 import { CommonModule } from '@angular/common';
 
@@ -14,6 +14,8 @@ export class ListaEstudiantes implements OnInit {
   cargando = true;             // Indicador de carga
   error: string | null = null; // Para manejar errores
 
+  private cdr = inject(ChangeDetectorRef);
+
   private estudianteService = inject(EstudianteService);
     ngOnInit() {
     this.estudianteService.obtenerEstudiantes().subscribe({
@@ -21,11 +23,13 @@ export class ListaEstudiantes implements OnInit {
         console.log(JSON.stringify(data));
         this.estudiantes = data;
         this.cargando = false;
+        this.cdr.markForCheck();
       },
       error: (err) => {
         console.error('Error al obtener estudiantes:', err);
         this.error = 'No se pudieron cargar los estudiantes.';
         this.cargando = false;
+        this.cdr.markForCheck();
       }
     });
   }
